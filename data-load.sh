5667fd3f2639cbfd2a5ac8423b91631bf13a4674
Mike Croft <backtotheoldhouse@gmail.com>
2021-06-14 10:39:29 +0100

    update helm chart and add data-reset.sh script to reset the game

--- data-load.sh
+++ data-load.sh
@@ -1,6 +1,10 @@
-curl -i -H "Content-Type:application/json" -d '{"name": "Iron Man", "power": "Being Rich", "weakness": "Magnets"}' http://localhost:8080/heroes
-curl -i -H "Content-Type:application/json" -d '{"name": "Thor", "power": "Lightning", "weakness": "Magic"}' http://localhost:8080/heroes
-curl -i -H "Content-Type:application/json" -d '{"name": "Superman", "power": "Strength", "weakness": "Kryptonite", "hp": "200"}' http://localhost:8080/heroes
-curl -i -H "Content-Type:application/json" -d '{"name": "Storm", "power": "Meteorology", "weakness": "Michael Fish"}' http://localhost:8080/heroes
-curl -i -H "Content-Type:application/json" -d '{"name": "Wonder Woman", "power": "Strength", "weakness": "Greek gods", "hp": "200"}' http://localhost:8080/heroes
-curl -i -H "Content-Type:application/json" -d '{"name": "Scarlet Witch", "power": "Magic", "weakness": "Dorothy`s house"}' http://localhost:8080/heroes
+export HOST=localhost:8080
+export ENDPOINT=heroes # Endpoint exposed automatically by HeroRepository
+
+curl -i -H "Content-Type:application/json" -d '{"name": "Iron Man", "power": "Being Rich", "weakness": "Magnets", "hp": "100"}' http://${HOST}/${ENDPOINT}
+curl -i -H "Content-Type:application/json" -d '{"name": "Thor", "power": "Lightning", "weakness": "Magic", "hp": "100"}' http://${HOST}/${ENDPOINT}
+curl -i -H "Content-Type:application/json" -d '{"name": "Superman", "power": "Strength", "weakness": "Kryptonite", "hp": "200"}' http://${HOST}/${ENDPOINT}
+curl -i -H "Content-Type:application/json" -d '{"name": "Storm", "power": "Meteorology", "weakness": "Michael Fish", "hp": "100"}' http://${HOST}/${ENDPOINT}
+curl -i -H "Content-Type:application/json" -d '{"name": "Wonder Woman", "power": "Strength", "weakness": "Greek gods", "hp": "200"}' http://${HOST}/${ENDPOINT}
+curl -i -H "Content-Type:application/json" -d '{"name": "Scarlet Witch", "power": "Magic", "weakness": "Dorothy`s house", "hp": "100"}' http://${HOST}/${ENDPOINT}
+curl -i -H "Content-Type:application/json" -d '{"name": "One Punch Man", "power": "Strength", "weakness": "None", "hp": "10000"}' http://${HOST}/${ENDPOINT}
